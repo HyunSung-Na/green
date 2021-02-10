b9a6453bb140a0f40c372a56d5b607a5543c6873
HyunSung-Na <59365947+HyunSung-Na@users.noreply.github.com>
2021-02-11 01:11:01 +0900

    user controller, service Test create

---
+++ src/test/java/com/green/demo/user/UserRestControllerTest.java
@@ -0,0 +1,141 @@
+package com.green.demo.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.green.demo.controller.user.SignUpRequest;
+import com.green.demo.error.NotFoundException;
+import com.green.demo.mail.EmailService;
+import com.green.demo.model.user.Email;
+import com.green.demo.model.user.Role;
+import com.green.demo.model.user.User;
+import com.green.demo.security.Jwt;
+import com.green.demo.service.user.UserService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UserRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private Jwt jwt;
+    @MockBean
+    EmailService emailService;
+
+    public static final Long ERROR_ID = 99L;
+
+    public static Long TEST_ID;
+
+    public static final String API_USER_URL = "/api/user/";
+
+    private String name;
+
+    private String password;
+
+    private String apiToken;
+
+    @BeforeAll
+    void setUp() {
+        name = "tester";
+        String email = "test@gmail.com";
+        password = "임의패스워드실험";
+
+        User user = userService.join(name, new Email(email), password);
+        apiToken = "Bearer "  + user.newApiToken(jwt, new String[]{Role.USER.value()});
+        TEST_ID = user.getSeq();
+    }
+
+    @Test
+    @Order(1)
+    void 회원가입() throws Exception{
+        String email = "oceana@email.com";
+        SignUpRequest signUpRequest = signUpRequest(email);
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(signUpRequest)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        User user = userService.findByEmail(new Email(email))
+                .orElseThrow(() -> new NotFoundException(User.class, email));
+
+        assertNotNull(user);
+        assertEquals(user.getName(), name);
+        assertEquals(user.getEmail(), new Email(email));
+        assertNotNull(user.getEmailCheckToken());
+    }
+
+    @Test
+    @Order(2)
+    void 회원조회() throws Exception{
+
+        mockMvc.perform(get(API_USER_URL + "info/" + TEST_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("api_key", apiToken))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    @Order(3)
+    void 회원목록조회() throws Exception{
+
+        Pageable page = PageRequest.of(0, 10);
+
+        mockMvc.perform(get(API_USER_URL + "info/list")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("page", String.valueOf(page.getPageNumber()))
+                .param("size", String.valueOf(page.getPageSize()))
+                .header("api_key", apiToken))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    @Order(4)
+    void 회원삭제() throws Exception{
+
+        mockMvc.perform(delete(API_USER_URL + "remove/" + TEST_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("api_key", apiToken))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @AfterAll
+    void After() {
+        userService.deleteById(TEST_ID);
+    }
+
+    private SignUpRequest signUpRequest(String email) {
+        return new SignUpRequest(name, email, password);
+    }
+
+}
--- src/test/java/com/green/demo/user/UserServiceTest.java
+++ src/test/java/com/green/demo/user/UserServiceTest.java
@@ -1,4 +1,91 @@
 package com.green.demo.user;
 
+import com.green.demo.error.NotFoundException;
+import com.green.demo.model.user.Email;
+import com.green.demo.model.user.User;
+import com.green.demo.service.user.UserService;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserServiceTest {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UserService userService;
+
+    private String name;
+
+    private Email email;
+
+    private String password;
+
+    @BeforeAll
+    void setUp() {
+        name = "tester";
+        email = new Email("test@gmail.com");
+        password = "임의패스워드실험";
+    }
+
+    @Test
+    @Order(1)
+    void 사용자를_추가한다() {
+        User user = userService.join(name, email, password);
+        assertThat(user, is(notNullValue()));
+        assertThat(user.getSeq(), is(notNullValue()));
+        assertThat(user.getEmail(), is(email));
+        log.info("Inserted user: {}", user);
+    }
+
+    @Test
+    @Order(2)
+    void 사용자를_이메일로_조회한다() {
+        User user = userService.findByEmail(email).orElse(null);
+        assertThat(user, is(notNullValue()));
+        assertThat(user.getEmail(), is(email));
+        log.info("Found by {}: {}", email, user);
+    }
+
+    @Test
+    @Order(3)
+    void 이메일로_로그인한다() {
+        User user = userService.login(email, password);
+        assertThat(user, is(notNullValue()));
+        assertThat(user.getEmail(), is(email));
+        assertThat(user.getLoginCount(), is(1));
+        log.info("First login: {}", user);
+
+        user = userService.login(email, password);
+        assertThat(user, is(notNullValue()));
+        assertThat(user.getEmail(), is(email));
+        assertThat(user.getLoginCount(), is(2));
+        log.info("Second login: {}", user);
+    }
+
+    @Test
+    @Order(4)
+    void 잘못된_비밀번호로_로그인을_할수없다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.login(email, "invalid password"));
+    }
+
+    @Test
+    @Order(5)
+    void 회원_탈퇴를_한다() {
+        User user = userService.findByEmail(email).orElseThrow(
+                () -> new NotFoundException(User.class, email));
+        userService.deleteById(user.getSeq());
+        assertThat(user, is(notNullValue()));
+    }
 }
