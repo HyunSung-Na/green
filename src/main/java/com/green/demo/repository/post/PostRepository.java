1636423f5de0d59161ca01b32859dd97440f243e
HyunSung-Na <59365947+HyunSung-Na@users.noreply.github.com>
2021-02-28 01:00:35 +0900

    post repository add

---
+++ src/main/java/com/green/demo/repository/post/PostRepository.java
@@ -0,0 +1,7 @@
+package com.green.demo.repository.post;
+
+import com.green.demo.model.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
