ed312cfa66e3f7762bb248a81b685dc4b7a88d99
HyunSung-Na <oceana57@naver.com>
2021-03-05 02:08:24 +0900

    header button add

--- green-frontend/src/components/header/header.jsx
+++ green-frontend/src/components/header/header.jsx
@@ -3,7 +3,8 @@ import styles from './header.module.css';
 
 const Header = memo(() => (
     <header className={styles.header}>
-        <button className={styles.logout}>LogIn</button>
+        <button className={styles.signUp}>회원가입</button>
+        <button className={styles.logout}>로그인</button>
         <img className={styles.logo} src="/images/logo.png" alt="logo"/>
         <h1 className={styles.title}>Green 그린이</h1>
     </header>
