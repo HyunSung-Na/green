b2c381f4cc4702d142d2df160e9949446cf46b7b
HyunSung-Na <oceana57@naver.com>
2021-03-18 01:27:34 +0900

    cors 설정

--- src/main/java/com/green/demo/configure/WebMvcConfigure.java
+++ src/main/java/com/green/demo/configure/WebMvcConfigure.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
