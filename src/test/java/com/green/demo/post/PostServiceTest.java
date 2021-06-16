a00ed514e9144480e96ccb06e6b33e926f4f9fb4
HyunSung-Na <oceana57@naver.com>
2021-06-17 00:35:32 +0900

    test: Post update, delete, read Test add

--- src/test/java/com/green/demo/post/PostServiceTest.java
+++ src/test/java/com/green/demo/post/PostServiceTest.java
@@ -2,6 +2,7 @@ package com.green.demo.post;
 
 import com.green.demo.controller.post.PostCreateDto;
 import com.green.demo.controller.post.PostDto;
+import com.green.demo.controller.post.PostUpdateDto;
 import com.green.demo.model.common.Name;
 import com.green.demo.model.user.Email;
 import com.green.demo.model.user.User;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -34,7 +36,7 @@ public class PostServiceTest {
     @Autowired
     private UserService userService;
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
         Name name = new Name("tester");
         Email email = new Email("test@gmail.com");
@@ -44,12 +46,52 @@ public class PostServiceTest {
 
     @Test
     @Order(1)
-    void 게시판작성() throws Exception {
+    void 게시판작성() {
         post = postService.write(createDto(), user.getEmail());
 
         assertThat(post, is(notNullValue()));
         assertThat(post.getId(), is(notNullValue()));
         log.info("Write post: {}", post);
+    }
+
+    @Test
+    @Order(2)
+    void 게시판수정() {
+        // given
+        PostUpdateDto postUpdateDto = new PostUpdateDto("수정제목", "수정내용");
+
+        // when
+        PostDto postDto = postService.updatePost(postUpdateDto, user.getEmail(), post.getId());
+
+        // then
+        assertThat(postDto.getTitle(), is("수정제목"));
+        assertThat(postDto.getContents(), is("수정내용"));
+    }
+
+    @Test
+    @Order(3)
+    void 게시판조회() {
+        // when
+        PostDto postDto = postService.postDetail(post.getId());
+
+        // then
+        assertThat(postDto.getTitle(), is("수정제목"));
+        assertThat(postDto.getContents(), is("수정내용"));
+    }
+
+    @Test
+    @Order(4)
+    void 게시판삭제() {
+        // when
+        postService.deletePost(post.getId(), user.getEmail());
+
+        // then
+        assertThat(postService.findByPostTitle(post.getTitle()), is(nullValue()));
+    }
+
+    @AfterAll
+    void after() {
+        userService.deleteById(user.getId());
     }
 
     private PostCreateDto createDto() {
