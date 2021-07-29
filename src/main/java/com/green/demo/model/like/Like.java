5e2eb4d6b4375544b37dfc7ea8bc62efb23bcf3d
HyunSung-Na <oceana57@naver.com>
2021-07-29 21:59:38 +0900

    feat: Like entity add

---
+++ src/main/java/com/green/demo/model/like/Like.java
@@ -0,0 +1,22 @@
+package com.green.demo.model.like;
+
+import com.green.demo.model.review.Review;
+import com.green.demo.model.user.User;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Like {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Review review;
+}
