624a74b3af1ed159aa73474cfb681c1cbdbdaed8
hyunsung <oceana57@naver.com>
2021-12-09 22:59:02 +0900

    feat: ServiceConfigure securityResourceService 빈 설정 추가

--- src/main/java/com/green/demo/configure/ServiceConfigure.java
+++ src/main/java/com/green/demo/configure/ServiceConfigure.java
@@ -1,24 +1,14 @@
 package com.green.demo.configure;
 
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.green.demo.repository.resources.ResourcesRepository;
 import com.green.demo.security.Jwt;
+import com.green.demo.service.resource.SecurityResourceService;
 import com.green.demo.util.MessageUtils;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.MessageSourceAccessor;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ServiceConfigure {
@@ -33,6 +23,11 @@ public class ServiceConfigure {
   @Bean
   public Jwt jwt(JwtTokenConfigure jwtTokenConfigure) {
     return new Jwt(jwtTokenConfigure.getIssuer(), jwtTokenConfigure.getClientSecret(), jwtTokenConfigure.getExpirySeconds());
+  }
+
+  @Bean
+  public SecurityResourceService securityResourceService(ResourcesRepository resourcesRepository) {
+    return new SecurityResourceService(resourcesRepository);
   }
 
 }
